85654d4bbbfe43898de76e69674af3d82e636f1b
Seowoo Nam <seowoonam@CFSN.home>
2024-10-17 13:08:17 +0200

    Force add the out directory

---
+++ out/_next/static/v5Ypi3qwMvpnGiwuOVp6x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-73091aa01ae51fe9.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/patterns/[slug]":["static/chunks/pages/patterns/[slug]-097ec895889fed07.js"],sortedPages:["/","/_app","/_error","/patterns/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
