da0775cf439ec9ad50fee8eef086236646519b27
Seowoo Nam <seowoonam@CFSN.home>
2024-10-17 13:09:36 +0200

    Updated project for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  /*reactStrictMode: true,*/
+  output: 'export',
+  basePath: '/design-pattern-library',  // This is your GitHub repository name
+  assetPrefix: '/design-pattern-library/',
 };
 
 export default nextConfig;
+
+/*module.exports = nextConfig*/
